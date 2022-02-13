8cb4c2e299237f5f9675836a284b8ea0fefa0099
callanbb <callan.brombacher@aogroup.co.za>
2022-02-13 13:32:15 +0200

    responsive changes to header and homepage

--- resources/js/components/pages/home.component.js
+++ resources/js/components/pages/home.component.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import {Button, Carousel, Form} from "react-bootstrap";
 import placeholder1 from "../../../images/coverImage.jpg"
+import {isMobile, isTablet} from "react-device-detect";
 
 export default function Home(){
     return(
-        <div className={"homepage"}>
-            <Carousel indicators={true} variant="dark">
-                <Carousel.Item>
-                    <img
-                        className="d-block w-auto h-50"
+        <div className={"homepage"} >
+                    { isMobile === true || isTablet === true ? (
+                        <Carousel indicators={true} variant="dark">
+                        <Carousel.Item>
+                        <img
+                        className="d-block w-100 h-auto"
                         src={placeholder1}
-                        alt="First slide"
-                    />
+                        alt="First slide" />
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-auto h-50"
+                        className="d-block w-100 h-auto"
                         src={placeholder1}
                         alt="Second slide"
                     />
@@ -23,14 +24,42 @@ export default function Home(){
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                        className="d-block w-auto h-50"
+                        className="d-block w-100 h-auto"
                         src={placeholder1}
                         alt="Third slide"
                     />
 
                 </Carousel.Item>
-            </Carousel>
+                        </Carousel>
+                        ) : (
+                        <Carousel indicators={true} variant="dark">
+                <Carousel.Item>
+                    <img
+                        className="d-block w-auto h-50"
+                        src={placeholder1}
+                        alt="First slide"
+                        />
+                </Carousel.Item>
+                        <Carousel.Item>
+                        <img
+                        className="d-block w-auto h-50"
+                        src={placeholder1}
+                        alt="Second slide"
+                        />
+
+                        </Carousel.Item>
+                        <Carousel.Item>
+                        <img
+                        className="d-block w-auto h-50"
+                        src={placeholder1}
+                        alt="Third slide"
+                        />
+
+                        </Carousel.Item>
+                        </Carousel>
+                        )}
             <Form>
+                <h3>Get Connected and ... <br/> contact us</h3>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
@@ -42,9 +71,6 @@ export default function Home(){
                 <Form.Group className="mb-3" controlId="formBasicText" >
                     <Form.Label>Message</Form.Label>
                     <Form.Control type="textArea" placeholder="Message" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
--- resources/js/components/shared_components/header.component.js
+++ resources/js/components/shared_components/header.component.js
@@ -1,18 +1,46 @@
-import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap'
+import React, {useState} from 'react';
+import {Navbar, Nav, Button, Offcanvas} from 'react-bootstrap'
 import logo from "/images/logo.png"
 import {Link} from "react-router-dom";
+import {isMobile, isTablet} from "react-device-detect";
 
 
 export default function Header(){
-    return(
+    const [show, setShow] = useState(false);
 
-    <div>
-            <Navbar bg="light" expand="lg">
-                    <img src={logo} alt=""  />
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+return (
+    isMobile===true || isTablet ===true ? (<div>
+        <Navbar bg="light" expand="lg">
+            <img src={logo} alt="" height={300} width="auto"/>
+            <Button variant="primary" onClick={handleShow}>
+                Menu
+            </Button>
+
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                    <Offcanvas.Title>Menu</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
                     <div className="nav">
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                        <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            navbarScroll>
+                            <Link className="route" to="/"><Nav.Link onClick={{handleClose}}>Home</Nav.Link></Link>
+                            <Link className="route" to="/"><Nav.Link onClick={{handleClose}}>Login</Nav.Link></Link>
+                        </Nav>
+                    </div>
+                </Offcanvas.Body>
+            </Offcanvas>
+        </Navbar>
+        </div>):(
+        <div>
+            <Navbar bg="light" expand="lg">
+                    <img src={logo} alt="" height={100} />
+                    <div className="nav">
                             <Nav
                                 className="me-auto my-2 my-lg-0"
                                 style={{ maxHeight: '100px' }}
@@ -20,9 +48,8 @@ export default function Header(){
                                 <Link className="route" to="/"><Nav.Link>Home</Nav.Link></Link>
                                 <Link className="route" to="/"><Nav.Link>Login</Nav.Link></Link>
                             </Nav>
-                        </Navbar.Collapse>
                     </div>
             </Navbar>
-    </div>
+    </div>)
     )
 }
